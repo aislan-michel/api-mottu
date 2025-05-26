b499011ed52209505e3de0b28ba2ac29a1833517
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2025-05-26 16:55:53 -0300

    test: delivery man entity

---
+++ tests/Mottu.Api.Tests/Domain/Entities/DeliveryManTests.cs
@@ -0,0 +1,19 @@
+using Mottu.Api.Domain.Entities;
+
+namespace Mottu.Api.Tests.Domain.Application
+{
+    public class DeliveryManTests
+    {
+        public void Create_ShouldInactive_WhenDriverLicenseImageIsNull()
+        {
+            var deliveryMan = new DeliveryMan(
+            "Fulano da Silva", "09556767000176",
+            new DateOnly(1999, 12, 28),
+            new DriverLicense("12227462477", "A", null),
+            Guid.NewGuid().ToString()
+            );
+
+            Assert.False(deliveryMan.Active);
+        }
+    }
+}
